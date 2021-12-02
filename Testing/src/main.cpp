00cddb0f5ad23c99740381a0a201931282380ea0
Georgi Jefimov <jefimov2003@gmail.com>
2021-12-02 11:38:32 +0200

    spin that bitch (angle sensor) with your hand

--- Testing/src/main.cpp
+++ Testing/src/main.cpp
@@ -1,22 +1,19 @@
-// apparently the most important shit idk arduino too yeah
-#include <Wire.h>
-#include <Arduino.h>
-// Include the Arduino Stepper Library
-#include <Stepper.h>
-
-// encoder
-#include <Encoder.h>
-
+#include <Wire.h> // apparently the most important shit idk arduino too yeah
+#include <Arduino.h>// Include the Arduino Stepper Library
+#include <Stepper.h> // stepper motor
+#include <Encoder.h> // encoder
 // Number of steps per output rotation
 const int stepsPerRevolution = 200;
-
 // Create Instance of Stepper library
 Stepper myStepper(stepsPerRevolution, 4, 5, 6, 7);
 
-#define CLK 11
-#define DT 12
+#define S1 11
+#define S2 12
 
-Encoder knob(CLK, DT);
+int prevS1;
+int curS1, curS2;
+
+int val = 0;
 
 void setup()
 {
@@ -24,43 +21,57 @@ void setup()
 	myStepper.setSpeed(60);
 	// initialize the serial port:
 	Serial.begin(9600);
-	Serial.println("TwoKnobs Encoder Test:");
+	pinMode (S1, INPUT);
+  pinMode (S2, INPUT);
+
+  prevS1 = digitalRead(S1);
 }
 
-
-long positionLeft  = -999;
-long positionRight = -999;
-
-void loop() 
+void loop()
 {
+  curS1 = digitalRead(S1);
+
+  if (curS2!=curS1) {
+    curS2 = digitalRead(S2);
+
+    if(curS2 == curS1){
+      val++;
+    }
+    else{
+      val--;
+    }
+    Serial.print("Value = ");
+    Serial.println(val);
+  }
+
+  prevS1=curS1;
+  
+  bool flag;
+
+  if(curS1!=prevS1){
+    curS2 = digitalRead(S2);
+    if(flag){
+      if(curS2==curS1){
+        val++;
+      }
+      else{
+        val--;
+      }
+      Serial.print("Value = ");
+      Serial.println(val);
+      flag=false;
+    }
+    else{
+      flag=true;
+    }
+  }
+  prevS1=curS1;
+
 	// step one revolution in one direction:
-	Serial.println("clockwise");
 	myStepper.step(stepsPerRevolution);
-	delay(500);
+	delay(1500);
 
 	// step one revolution in the other direction:
-	Serial.println("counterclockwise");
 	myStepper.step(-stepsPerRevolution);
-	delay(500);
-
-	long newLeft, newRight;
-  newLeft = knob.read();
-  newRight = knob.read();
-  if (newLeft != positionLeft || newRight != positionRight) {
-    Serial.print("Left = ");
-    Serial.print(newLeft);
-    Serial.print(", Right = ");
-    Serial.print(newRight);
-    Serial.println();
-    positionLeft = newLeft;
-    positionRight = newRight;
-  }
-  // if a character is sent from the serial monitor,
-  // reset both back to zero.
-  if (Serial.available()) {
-    Serial.read();
-    Serial.println("Reset both knobs to zero");
-    knob.write(0);
-    knob.write(0);
-  }
+	delay(1500);
 }
